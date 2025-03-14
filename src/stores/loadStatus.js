4d45dcb185e55462f514756d1bd9a4c4e4e62afb
Denis <denisdamse@gmail.com>
2025-03-14 15:14:00 +0200

    added actions

--- src/stores/loadStatus.js
+++ src/stores/loadStatus.js
@@ -30,5 +30,29 @@ export const useLoadStatus = defineStore('loadStatus', {
         }
     },
     actions: {
+        leaveForLoading() {
+            this.isLeftForLoading.checked = true;
+            this.isLeftForLoading.tickVisibility = true;
+            this.isArrivedAtLoading.isDisabled = false;
+        },
+        arriveAtLoading() {
+            this.isArrivedAtLoading.checked = true;
+            this.isArrivedAtLoading.tickVisibility = true;
+            this.isLoaded.isDisabled = false;
+        },
+        load() {
+            this.isLoaded.checked = true;
+            this.isLoaded.tickVisibility = true;
+            this.isArrivedAtUnloading.isDisabled = false;
+        },
+        arriveAtUnloading() {
+            this.isArrivedAtUnloading.checked = true;
+            this.isArrivedAtUnloading.tickVisibility = true;
+            this.isUnloaded.isDisabled = false;
+        },
+        unload() {
+            this.isUnloaded.checked = true;
+            this.isUnloaded.tickVisibility = true;
+        }
     }
 })
