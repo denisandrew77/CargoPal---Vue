b48d4c72ae87d63016b9a6016f70a5e071494c8a
Denis <denisdamse@gmail.com>
2025-03-17 15:47:22 +0200

    added routing for company create account

--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,45 @@
 import { createApp } from 'vue'
-import {createRouter,createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Decidingpage from '../components/DriverPages/Decidingpage.vue'
 import DriverSignIn from '../components/DriverPages/DriverSignIn.vue'
 import Loading from '../components/DriverPages/Loading.vue';
 import Delivery from '../components/DriverPages/Delivery.vue';
 import OrderDone from '../components/DriverPages/OrderDone.vue';
+import CreateCompanyAccount from '../components/CompanyPages/CreateCompanyAccount.vue';
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
-    {
-    path: '/',
-    name: 'DecidingPage',
-    component: Decidingpage
-    },
-    {
-    path: '/driverSignIn',
-    name: 'DriverSignIn',
-    component: DriverSignIn
-    },
-    {
-    path: '/loading',
-    name: 'Loading',
-    component: Loading
-    },
-    {
-        path: '/delivery',
-        name: 'Delivery',
-        component: Delivery
+        {
+            path: '/',
+            name: 'DecidingPage',
+            component: Decidingpage
         },
-    {
-        path: '/orderDone',
-        name: 'OrderDone',
-        component: OrderDone
-        },    
-        
+        {
+            path: '/driverSignIn',
+            name: 'DriverSignIn',
+            component: DriverSignIn
+        },
+        {
+            path: '/loading',
+            name: 'Loading',
+            component: Loading
+        },
+        {
+            path: '/delivery',
+            name: 'Delivery',
+            component: Delivery
+        },
+        {
+            path: '/orderDone',
+            name: 'OrderDone',
+            component: OrderDone
+        },
+        {
+            path: '/createCompanyAccount',
+            name: 'CreateCompanyAccount',
+            component: CreateCompanyAccount
+        }
     ],
-    })
+})
 
 export default router;
